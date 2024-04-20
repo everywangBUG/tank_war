5008771e697b3070ca09662d01c9ccb6c21fc9fc
王政 <everywangbug@gmail.com>
2024-04-21 01:09:01 +0800

    write text in dashboard

--- tank_war/src/main.ts
+++ tank_war/src/main.ts
@@ -2,14 +2,38 @@ import './global.css'
 class BlackBoard {
     constructor(
       public el =  document.getElementById("dashboard") as HTMLCanvasElement,
-      private ctx = el.getContext("2d"),
+      private ctx = el.getContext("2d")!,
       private width : number = el.width,
       private height: number = el.height,
     ) {
-      if (this.ctx) {
-        this.ctx.fillStyle = "black"
-        this.ctx?.fillRect(0, 0, this.width, this.height)
-      }
+      this.init()
+      this.bindEvent()
+    }
+    
+    private init() {
+      this.ctx.fillStyle = "black"
+      this.ctx?.fillRect(0, 0, this.width, this.height)
+    }
+
+    private bindEvent() {
+      // 鼠标移动
+      const callback = this.drawLine.bind(this)
+      this.el.addEventListener("mousedown", () => {
+        // 绘制
+          this.ctx.beginPath()
+          this.ctx.strokeStyle = "white"
+          this.el.addEventListener('mousemove', callback)
+          // 鼠标松开结束划线
+          // 使用document，防止事件冒泡在canvas外部松开不能移除事件
+          document.addEventListener('mouseup', () => {
+            this.el.removeEventListener('mousemove', callback)
+          })
+      })
+    }
+    
+    private drawLine(e: MouseEvent) {
+      this.ctx.lineTo(e.offsetX, e.offsetY)
+      this.ctx.stroke()
     }
 }
 
